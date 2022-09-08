bffc507d7ff3961eafda0059ac18b0f12daef400
felix <felix.okoronkwo.1@gmail.com>
2022-09-08 23:49:07 +0000

    low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-/*main is entry point*/
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char text[] = "\"Programming is like building a multilingual puzzle\n";
+	char text[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
 	return (0);
