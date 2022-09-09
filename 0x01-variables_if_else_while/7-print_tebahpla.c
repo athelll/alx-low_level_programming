e66d0cb515413ab3207b919fdf1e9645e1ff9327
felix <felix.okoronkwo.1@gmail.com>
2022-09-10 00:56:25 +0100

    low_level_programming

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char alpha = 'z';
+
+for (; alpha <= 'z'; alpha--)
+{
+putchar (alpha);
+}
+putchar ('\n');
+
+return (0);
+}
