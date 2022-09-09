884d1048935c69b9f3a1651e4163d553e5d51e04
felix <felix.okoronkwo.1@gmail.com>
2022-09-09 00:22:52 +0000

    low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	return (0);
 }
