032e70a027a17ae60156f681cae6b15b4f8403fd
felix <felix.okoronkwo.1@gmail.com>
2022-09-10 00:18:06 +0100

    low_level_programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: returns 0 SUCCESS
+ */
+
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+putchar(a);
+}
+putchar(`\n`);
+
+return 0
+}
