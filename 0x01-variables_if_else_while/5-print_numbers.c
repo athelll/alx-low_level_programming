2585c2975c253695673e589f9f8c128bfeb17612
felix <felix.okoronkwo.1@gmail.com>
2022-09-10 01:17:47 +0100

    low_level_programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+/**                                               * main - entry point
+* Return: Always 0.                             
+*/
+                                                  int main(void)
+{
+int a;    
+
+for (a = 0; a < 10; a++)              
+{
+printf ("%i", a);
+}
+putchar('\n');    
+
+return (0);
+}
