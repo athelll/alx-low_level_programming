b941c90af8dcff6585151bd738b5e43afc1b4bcf
felix <felix.okoronkwo.1@gmail.com>
2022-09-11 23:11:25 +0000

    low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
 *main - Prints all possible combinations of two two-digit numbers,
- ranging from 0-99, separated by a comma followed by a space.
+*ranging from 0-99, separated by a comma followed by a space.
 *Return: Always 0.
 */
 
