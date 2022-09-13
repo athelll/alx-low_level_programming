e234793678b24bbd54d637c6c5e1bf3c17a19519
felix <felix.okoronkwo.1@gmail.com>
2022-09-13 19:40:30 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,11 @@
 
 int print_last_digit(int num)
 {
-int num_last;
-
 if (num < 0)
 {
 num *= -1;
 }
-num_last = num % 10;
-_putchar('0' + num_last);
+_putchar('0' + (num % 10));
 
-return (num_last);
+return (num % 10);
 }
