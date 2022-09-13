9ca9ac66d4cb2f31f9c3ea1e9ba1f91d4ceda7a7
felix <felix.okoronkwo.1@gmail.com>
2022-09-13 14:57:08 +0000

    functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include "main.h"
+i
+/**
+* _putchar - Prints single character
+* @c: First operand
+*
+* Description: This function prints the operand c
+* Return: Always 0.
+*/
+
+int _putchar (char c)
+{
+putchar(c);
+
+return (0);
+}
+
+/**
+* main - Entry point
+* Return: Always 0.
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions and nested loops in C
