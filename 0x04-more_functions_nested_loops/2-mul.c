e05be268464602f4f8bf7affc7d314bedc0ab8a3
felix <felix.okoronkwo.1@gmail.com>
2022-09-15 11:12:19 +0000

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul- mutiplies two functions together
+* @a: Parameter 1
+* @b: Parameter 2
+* Return: returns the mutiplication of the parameters
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
