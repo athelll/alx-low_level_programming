c7176bafff9798dcada7e8edd621442da5f9fce5
felix <felix.okoronkwo.1@gmail.com>
2022-09-22 12:10:38 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,20 +4,23 @@
 * _strcat- Concatenate 2 strings
 * @dest: Parameter
 * @src: Second Parameter
+* @n: Third Parameter
 * Return: returns dest.
 */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 int a = 0;
 int b = 0;
 
 while (dest[a] != '\0')
-dest++;
+a++;
 
-while (src[b] != '\0')
+while (src[b] != src[n])
 {
 dest[a] = src[b];
+if (src[b] == '\0')
+break;
 a++;
 b++;
 }
