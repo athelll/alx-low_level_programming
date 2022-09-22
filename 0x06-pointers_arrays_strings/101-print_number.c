dcaab5c93002477d9b17950ea225f656f2b046fa
felix <felix.okoronkwo.1@gmail.com>
2022-09-22 18:30:45 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,37 +1,30 @@
 #include "main.h"
 
-/**
-* print_number - prints an integer
-* @n: input integer
-* Return: no return
-*/
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 
+/**
+* print_number - using only _putchar
+* @n: the integer to print
+*
+* Return: void
+*/
 void print_number(int n)
 {
-unsigned int m, d, count;
+int i, p;
 
 if (n < 0)
+_putchar('-');
+for (p = 0, i = 1000000000; i > 0; i /= 10)
 {
-_putchar(45);
-m = n * -1;
-}
-else
+int d = (n / i) % 10;
+
+d = d < 0 ? -d : d;
+if (d || p || i == 1)
 {
-m = n;
-}
-
-d = m;
-count = 1;
-
-while (d > 9)
-{
-d /= 10;
-
-count *= 10;
+_putchar(d + '0');
+p++;
 }
 
-for (; count >= 1; count /= 10)
-{
-_putchar (((m / count) % 10) + 48);
 }
 }
