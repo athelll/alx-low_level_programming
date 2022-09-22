f864223b71f9f5a74e37727e1faa40c8b2654b87
felix <felix.okoronkwo.1@gmail.com>
2022-09-22 14:07:44 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-if (s1 < s2)
-return (-15);
-else if (s1 > s2)
-return (15);
-else
-return (0);
+int a = 0;
+int b = 0;
+
+while(a == 0)
+{
+if (s1[b] == '\0' && s2[b] == '\0') 
+break;
+a = s1[b] - s2[b];
+b++;
+}
+
+return (a);
 }
