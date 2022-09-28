73e03804423b8a589d24cd9507051f323844daf0
felix <felix.okoronkwo.1@gmail.com>
2022-09-28 18:35:50 +0000

    Recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion- function puts but with recursion
+* @s: Parameter 1
+* Return: Void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char s);
+void _puts_recursion(char *s);
+
+#endif
