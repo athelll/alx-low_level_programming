5496218ba97d2b4facac28c4f51230d18b4825c0
felix <felix.okoronkwo.1@gmail.com>
2022-09-30 11:30:46 +0000

    Argument Count and Vectors

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the output file that runs the code
+* @argc : Argument Count
+* @argv : Argument Vector
+* Return : returns void.
+*/
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
