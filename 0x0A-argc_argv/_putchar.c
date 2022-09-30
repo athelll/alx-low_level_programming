d2ece2d1e62cb553caa4bfa5a7c76e90e4eb48c6
felix <felix.okoronkwo.1@gmail.com>
2022-09-30 11:54:20 +0000

    Argument Count and Vectors

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
 /**
 * _putchar - writes the character c to stdout
@@ -9,6 +8,7 @@
 * On error, -1 is returned, and errno is set appropriately.
 */
 
-int _putchar(char c);
-
-#endif
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
