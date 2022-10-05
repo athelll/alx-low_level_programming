d9629f5c521bb2fb229932f1e723d76aaad3cfb3
felix <felix.okoronkwo.1@gmail.com>
2022-10-05 20:53:58 +0000

    Malloc and Free in C

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_array - creates an array
+* @size: Size parameter
+* @c: Character parameter
+* Return: returns an array or NULL if failed.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+
+	a[i] = '\0';
+	return (a);
+}
