ee597e5dede1b60c01edf6b7a386f48d6cc89ada
felix <felix.okoronkwo.1@gmail.com>
2022-10-05 21:43:53 +0000

    Malloc and Free in C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	c = malloc(sizeof(char) * strlen(str));
+	c = malloc((sizeof(char) * strlen(str)) + 1);
 
 	if (c == NULL)
 		return (NULL);
