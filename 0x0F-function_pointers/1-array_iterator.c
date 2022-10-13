027ba0584fe7f9a4f7e1fd3bdf2ed5905a95b9a5
felix <felix.okoronkwo.1@gmail.com>
2022-10-13 01:17:30 +0000

    Function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+  * array_iterator - iterates an array an applys an function on it
+  * @array: array in which the function will be applied
+  * @size: supposed size of the array;
+  * @action: specified function to be performed
+  * Return: returns void.
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i, b = size;
+	void (*func)(int);
+
+	func = action;
+
+	for (i = 0; i < b; i++)
+	{
+		func(array[i]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * int_index - searches for an integer using a functions logic
+  * @array: array in which number to be searched is stored
+  * @size: size of array
+  * @cmp: compare function.
+  * Return: returns an int.
+  */
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int (*num)(int);
+
+	if (size <= 0)
+		return (-1);
+	num = cmp;
+
+	for (i = 0; i < size; i++)
+	{
+		if (num(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
