dd99feff23ec69f701aae7928455f9946f9b3a40
felix <felix.okoronkwo.1@gmail.com>
2022-10-21 20:25:51 +0000

    Linked List

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *tmp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (str == NULL || strdup(str) == NULL)
 		return (NULL);
 
-	if (str == NULL || strdup(str) == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
