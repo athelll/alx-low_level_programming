b64da8446b79bdeb888be5f431efa97f0ce68780
felix <felix.okoronkwo.1@gmail.com>
2022-10-29 04:38:33 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * get_endianness - checks if multibyte data is stored as little or big endian
+  * Return: first value stored, 0 if big endian, 1 if little
+  */
+
+int get_endianness(void)
+{
+	int n = 0x0000000001;
+	char *c = (char *)&n;
+
+	return (c[0]);
+}
