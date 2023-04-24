d4c04d5a9f4fe47bc3e13396a605763ecef339e8
6felix <felix.okoronkwo.1@gmail.com>
2023-04-24 09:28:36 +0000

    prints elements in a linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,13 +14,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		len++;
 		h = h->next;
+		len++;
 	}
-	printf("%d\n", h->n);
-	len++;
+
 	return (len);
 }
