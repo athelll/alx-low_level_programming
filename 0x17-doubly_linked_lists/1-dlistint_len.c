818c2a1ffd40ba30cb0bc4be243c44c78ccc993a
6felix <felix.okoronkwo.1@gmail.com>
2023-04-24 09:44:06 +0000

    returns length of list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,17 +2,16 @@
 
 /**
  * dlistint_len - returns length of list
- * Return: returns number of elements
  * @h : doubly linked list pointer
+ * Return: returns number of elements
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
 	while (h)
 	{
-		h = h->n;
+		h = h->next;
 		nodes++;
 	}
 
