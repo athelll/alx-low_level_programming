de8307aff9b230efc79a2d821f24ab94a43d572f
6felix <felix.okoronkwo.1@gmail.com>
2023-04-26 21:16:01 +0000

    bash code that creates dynamic library

---
+++ 0x18-dynamic_libraries/test/libdynamic.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _putchar(char c) { return 0; }
+int _islower(int c) { return 0; }
+int _isalpha(int c) { return 0; }
+int _abs(int n) { return 0; }
+int _isupper(int c) { return 0; }
+int _isdigit(int c) { return 0; }
+int _strlen(char *s) { return 0; }
+void _puts(char *s) { return; }
+char *_strcpy(char *dest, char *src) { return "strcpy"; }
+int _atoi(char *s) { return 0; }
+char *_strcat(char *dest, char *src) { return "strcat"; }
+char *_strncat(char *dest, char *src, int n) { return "strncat"; }
+char *_strncpy(char *dest, char *src, int n) { return "strncpy"; }
+int _strcmp(char *s1, char *s2) { return 0; }
+char *_memset(char *s, char b, unsigned int n) { return "memeset"; }
+char *_memcpy(char *dest, char *src, unsigned int n) { return "memcpy"; }
+char *_strchr(char *s, char c) { return "strchr"; }
+unsigned int _strspn(char *s, char *accept) { return 1; }
+char *_strpbrk(char *s, char *accept) { return "strpbrk"; }
+char *_strstr(char *haystack, char *needle) { return "strstr"; }
