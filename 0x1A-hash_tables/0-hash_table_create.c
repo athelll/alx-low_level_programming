d55bf9774fab6784a6d96d4a847f1f9ac1612b7c
6felix <felix.okoronkwo.1@gmail.com>
2023-04-27 21:20:09 +0000

    creates new hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 
+	/** check if size is null **/
+	if (!size)
+	{
+		free(table);
+		return (NULL);
+	}
+
 	table->size = size;
 
 	/** returns pointer to new hash table **/
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
