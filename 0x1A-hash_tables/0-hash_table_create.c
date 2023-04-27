ba99b6042dee28f103d96986fecdf5ec2cbed19f
6felix <felix.okoronkwo.1@gmail.com>
2023-04-27 21:13:53 +0000

    function creates a new hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * Return: returns a pointer to the hash table
+ *
+ * @size: initializes the size of the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
+	/** checks if table is null **/
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+
+	/** returns pointer to new hash table **/
+	return (table);
+}
